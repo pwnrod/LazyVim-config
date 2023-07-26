ce2094dc241ee6831a0beef236ab5e3f5e27a595
Caylin James <cjames@leapagency.com>
2023-07-26 14:52:54 -0400

    switch color scheme again, remove eob fillchars and invisible chars

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,5 @@
 -- Add any additional options here
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.fillchars = { eob = " " }
+vim.opt.list = false
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,10 +2,11 @@ return {
   { "catppuccin/nvim" },
   { "folke/tokyonight.nvim" },
   { "rebelot/kanagawa.nvim" },
+  { "rose-pine/neovim", name = "rose-pine", variant = "moon" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "rose-pine",
     },
   },
 }
