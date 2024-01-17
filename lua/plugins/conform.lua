81ba3ba935327bcc5d76c7c1c521996918b30fd7
Caylin James <caylinjames.dev@gmail.com>
2024-01-17 08:19:43 -0500

    initial commit
    basic configuration of new neovim setup with LazyVim, using the nightfox color theme

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+    "stevearc/conform.nvim",
+    opts = function()
+        local opts = {
+            formatters_by_ft = {
+                javascript = { { "prettierd", "prettier" } },
+            },
+        }
+        return opts
+    end,
+}
---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,53 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("harpoon"):setup()
+    end,
+    keys = {
+        {
+            "<leader>a",
+            function()
+                require("harpoon"):list():append()
+            end,
+            desc = "Harpoon current file",
+        },
+        {
+            "<leader>h",
+            function()
+                local harpoon = require("harpoon")
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end,
+            desc = "Harpoon quick menu",
+        },
+        {
+            "<C-h>",
+            function()
+                require("harpoon"):list():select(1)
+            end,
+            desc = "Harpoon file 1",
+        },
+        {
+            "<C-j>",
+            function()
+                require("harpoon"):list():select(2)
+            end,
+            desc = "Harpoon file 2",
+        },
+        {
+            "<C-k>",
+            function()
+                require("harpoon"):list():select(3)
+            end,
+            desc = "Harpoon file 3",
+        },
+        {
+            "<C-l>",
+            function()
+                require("harpoon"):list():select(4)
+            end,
+            desc = "Harpoon file 4",
+        },
+    },
+}
