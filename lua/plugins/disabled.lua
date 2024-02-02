54149d84da43fd56f855520bf33de3d9128d2df9
Caylin James <caylinjames.dev@gmail.com>
2024-02-01 23:29:42 -0500

    set up tailwind-sorter for sorting tailwind classes

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,10 +1,9 @@
 return {
-    { "akinsho/bufferline.nvim",                 enabled = false },
-    { "folke/flash.nvim",                        enabled = false },
-    { "echasnovski/mini.indentscope",            enabled = false },
-    { "nvim-neo-tree/neo-tree.nvim",             enabled = false },
-    { "rcarriga/nvim-notify",                    enabled = false },
-    { "nvim-pack/nvim-spectre",                  enabled = false },
-    { "nvim-treesitter/nvim-treesitter-context", enabled = false },
-    { "RRethy/vim-illuminate",                   enabled = false },
+    { "akinsho/bufferline.nvim",      enabled = false },
+    { "folke/flash.nvim",             enabled = false },
+    { "echasnovski/mini.indentscope", enabled = false },
+    { "nvim-neo-tree/neo-tree.nvim",  enabled = false },
+    { "rcarriga/nvim-notify",         enabled = false },
+    { "nvim-pack/nvim-spectre",       enabled = false },
+    { "RRethy/vim-illuminate",        enabled = false },
 }
